1cb7b9c0416e9d4a95f62374e61f4f3c9469fec4
Harshit242005 <agreharshit610@gmail.com>
2024-02-19 18:22:37 +0530

    Understanding about the Entity Framework and creating one on the docker based mysql database with initial Users named table

---
+++ Razor Applications/Reachbox/Pages/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
--- Razor Applications/Reachbox/Program.cs
+++ Razor Applications/Reachbox/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add your DbContext and connection string here
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql("Server=localhost;Port=3307;Database=reachbox;User=root;Password=reachbox;", 
+    new MySqlServerVersion(new Version(8, 0, 22))));
+
 
 var app = builder.Build();
 
