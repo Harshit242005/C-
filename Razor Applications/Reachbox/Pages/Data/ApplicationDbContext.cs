b45f940304869dce6dda93dbfacf46a1c0d06e27
Harshit242005 <agreharshit610@gmail.com>
2024-03-24 11:12:04 +0530

    New razor mvc based application

--- Razor Applications/Reachbox/Pages/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<User> Users { get; set; }
-}
--- Razor Applications/Reachbox/Python/mongo_connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def get_mongo_connection(database_name, collection_name):
-    # Replace 'your_database_uri' with your MongoDB connection string
-    client = MongoClient('mongodb://localhost:27017/')
-    # Access a specific database, replace 'your_database_name' with your database name
-    db = client[database_name]
-    collection = db[collection_name]
-    return collection
